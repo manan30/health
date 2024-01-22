0c25e0581556d891b9e0d3f594c5aa24640ff36d
Manan Joshi <mananjoshi1995@gmail.com>
2024-01-21 23:32:00 -0600

    feat: adding fitness schema

---
+++ packages/db/src/schema/fitness/index.ts
@@ -0,0 +1,6 @@
+export * from "./workout";
+export * from "./exercise";
+export * from "./exercise-type";
+export * from "./muscle-group";
+export * from "./equipment";
+export * from "./junction";
---
+++ packages/db/src/schema/fitness/workout.ts
@@ -0,0 +1,17 @@
+import { relations } from "drizzle-orm";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { exercise } from "./exercise";
+
+export const workout = pgTable("workout", {
+  id: serial("id").primaryKey().notNull(),
+  name: text("name").notNull(),
+  description: text("description"),
+  date: timestamp("date", { precision: 3, mode: "string" }).notNull(),
+  createdAt: timestamp("created_at", { precision: 3, mode: "string" })
+    .defaultNow()
+    .notNull(),
+});
+
+export const workoutRelations = relations(workout, ({ many }) => ({
+  exercises: many(exercise),
+}));
