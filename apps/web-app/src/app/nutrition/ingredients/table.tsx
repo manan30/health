2d7b2e6c24e125d09df9b4a76f48a037ead98b08
Manan Joshi <mananjoshi1995@gmail.com>
2024-03-10 22:46:33 -0500

    feat: fixing table styles

--- apps/web-app/src/app/nutrition/ingredients/table.tsx
+++ apps/web-app/src/app/nutrition/ingredients/table.tsx
@@ -60,7 +60,10 @@ export function DataTable<TData, TValue>({
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={header.id}>
+                    <TableHead
+                      key={header.id}
+                      className="text-secondary-foreground whitespace-nowrap"
+                    >
                       {header.isPlaceholder
                         ? null
                         : flexRender(
@@ -81,7 +84,7 @@ export function DataTable<TData, TValue>({
                   data-state={row.getIsSelected() && "selected"}
                 >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
+                    <TableCell key={cell.id} className="font-normal text-xs">
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
