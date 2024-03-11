533d985956645fe9a80f0d9cfe3bbd4af89e410c
Manan Joshi <mananjoshi1995@gmail.com>
2024-03-10 22:59:58 -0500

    feat: adding root page

---
+++ apps/web-app/src/app/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+import { Card, CardDescription, CardTitle } from "~/components/ui/card";
+
+export default function RootPage() {
+  return (
+    <div className="p-6">
+      <div className="flex flex-col space-y-1.5">
+        <h1 className="text-2xl font-semibold whitespace-nowrap leading-none tracking-tight">
+          Health and Fitness
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          My personal health and fitness tracker
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-8">
+        <Link href="/nutrition/ingredients">
+          <Card className="p-4 flex flex-col space-y-1.5 hover:bg-muted/50">
+            <CardTitle>Ingredients</CardTitle>
+            <CardDescription>Manage your ingredients</CardDescription>
+          </Card>
+        </Link>
+      </div>
+    </div>
+  );
+}
