f7505a1e354fa0361bead89fb24489b5cb151dfe
Manan Joshi <mananjoshi1995@gmail.com>
2024-04-10 22:15:53 -0500

    feat: db changes

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,14 +1,15 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-import * as schema from './schema';
+import * as schema from "./schema";
 
-export * from 'drizzle-orm';
-export * from './types';
+export * from "drizzle-orm";
+export * from "./types";
 export { schema };
 
 export function setupDb(dbUrl: string) {
-  const sql = neon(dbUrl);
-  const db = drizzle(sql, { schema, logger: true });
-  return db;
+	console.log({ dbUrl });
+	const sql = neon(dbUrl);
+	const db = drizzle(sql, { schema, logger: true });
+	return db;
 }
