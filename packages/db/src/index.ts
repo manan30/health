2e83df97aa5b8adccb0cd87de54371ca8235bde1
Manan Joshi <mananjoshi1995@gmail.com>
2024-04-15 23:28:38 -0500

    feat: adding lint step

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -7,8 +7,6 @@ export * from 'drizzle-orm';
 export * from './types';
 export { schema };
 
-console.log({ schema, neon });
-
 export function setupDb(dbUrl: string) {
 	const sql = neon(process.env.DRIZZLE_DATABASE_URL ?? '');
 	const db = drizzle(sql, { schema, logger: true });
