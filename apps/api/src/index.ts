cc2d5b6cfe82a43f028f0b960c11789620681428
Manan Joshi <mananjoshi1995@gmail.com>
2024-04-16 23:05:50 -0500

    feat: testing api action

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -20,7 +20,7 @@ app.notFound((c) => {
 });
 
 app.onError((err, c) => {
-	console.error(`Internal Server Error: ${err}`);
+	console.error(`${err}`);
 	return c.text('Internal Server Error', 500);
 });
 
